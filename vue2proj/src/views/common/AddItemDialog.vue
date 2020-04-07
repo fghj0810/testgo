83b07b16658b2a57d40ad1f698b5a77d596a1036
jie <fghj0810@gmail.com>
2020-04-07 23:49:05 +0800

    邮件

---
+++ vue2proj/src/views/common/AddItemDialog.vue
@@ -0,0 +1,51 @@
+<template>
+    <el-dialog title="添加道具" :visible.sync="dVisible" width="30%">
+        <div><span>道具ID：</span>
+            <el-input v-model="itemId" placeholder="请输入道具ID"></el-input>
+        </div>
+        <div><span>数量：</span>
+            <el-input v-model="itemNum" placeholder="请输入道具数量"></el-input>
+        </div>
+        <span slot="footer" class="dialog-footer">
+             <el-button @click="handleCancelClicked">取 消</el-button>
+             <el-button type="primary" @click="handleOKClicked" :disabled="dataInvalid">确 定</el-button>
+        </span>
+    </el-dialog>
+</template>
+
+<script>
+    export default {
+        name: "AddItemDialog",
+        props: {
+            dialogVisible: Boolean
+        },
+        data() {
+            return {
+                dVisible: this.dialogVisible,
+                itemId: '',
+                itemNum: '',
+                dataInvalid: false
+            }
+        },
+        methods: {
+            handleCancelClicked() {
+                this.$emit('update:dialogVisible', false)
+            },
+            handleOKClicked() {
+                this.$emit('update:dialogVisible', false)
+                var arg = { itemId:this.itemId, itemNum:this.itemNum}
+                this.$emit('onAddItem', arg)
+                console.log(arg.itemId)
+            }
+        },
+        watch: {
+            dialogVisible(val) {
+                this.dVisible = val
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue2proj/src/views/mail/SendMailToOneUser.vue
+++ vue2proj/src/views/mail/SendMailToOneUser.vue
@@ -1,42 +1,58 @@
 <template>
     <div align="left">
-        <el-input v-model="userId" placeholder="用户ID"></el-input>
-        <el-input v-model="title" placeholder="邮件标题"></el-input>
+        <div>
+            <el-input v-model="userId" placeholder="用户ID" style="width: 150px"></el-input>
+        </div>
+        <div>
+            <el-input v-model="title" placeholder="邮件标题"></el-input>
+        </div>
         <el-input
                 type="textarea"
-                :rows="20"
+                :rows="10"
                 placeholder="请输入邮件内容"
                 v-model="context">
         </el-input>
-        <div>
-            <span>附件</span>
-            <el-table :data="items" style="width: 460px">
-                <el-table-column prop="itemId" label="道具ID" width="180">
-                </el-table-column>
-                <el-table-column prop="itemNum" label="道具数量" width="180">
-                </el-table-column>
-                <el-table-column fixed="right" label="操作" width="100">
-                    <template slot-scope="scope">
-                        <el-button size="mini" type="danger" @click.native.prevent="handleDelete(scope.$index, scope.row)">
-                            <span>删除</span>
-                        </el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
+        <el-switch v-model="hasItems" active-text="有附件" inactive-text="无附件">
+        </el-switch>
+        <el-table :data="items" style="width: 460px" v-show="hasItems">
+            <el-table-column prop="itemId" label="道具ID" width="180">
+            </el-table-column>
+            <el-table-column prop="itemNum" label="道具数量" width="180">
+            </el-table-column>
+            <el-table-column label="操作" width="100">
+                <template slot="header">
+                    <el-button @click.native.prevent="addItemDialogVisible = true" type="primary"
+                               icon="el-icon-plus">
+                    </el-button>
+                </template>
+                <template slot-scope="scope">
+                    <el-button @click.native.prevent="handleDelete(scope.$index, scope.raw)" type="danger">
+                        移除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div style="margin-top: 50px">
+            <el-button @click="handleSendMsgClicked">发送</el-button>
         </div>
-        <el-button @click="handleSendMsgClicked">发送</el-button>
+        <add-item-dialog :dialog-visible.sync="addItemDialogVisible" v-on:onAddItem="handleAddItem" />
     </div>
 </template>
 
 <script>
+    import AddItemDialog from "../common/AddItemDialog";
     export default {
         name: "SendMailToOneUser",
+        components: {AddItemDialog},
+        comments : {AddItemDialog},
         data() {
             return {
                 userId: '',
                 title: '',
                 context: '',
-                items: [{itemId: 1, itemNum: 10}, {itemId: 2, itemNum: 10}]
+                hasItems: false,
+                items: [{itemId: 1, itemNum: 10}, {itemId: 2, itemNum: 10}, {itemId: 2, itemNum: 10}],
+                addItemDialogVisible : false
             }
         },
         methods: {
@@ -46,6 +62,10 @@
             },
             handleSendMsgClicked(event) {
                 console.log(event);
+            },
+            handleAddItem(event) {
+                console.log(event);
+                event.itemId = 100
             }
         }
     }
